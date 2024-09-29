819791a21cdc79e1e19a8a3d6fd49bd299a18127
Tiago Castro <tiagocastro044@gmail.com>
2024-09-29 14:46:56 +0100

    Aula 1 - Assinatura, region e introdução ao C#

---
+++ Aula 1/Program.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------
+//    <copyright file="Aula_1.cs"    company="IPCA">
+//     Copyright (c) IPCA-EST 2024. All rights reserved.
+//    </copyright>
+//    <date>2024-09-26</date>
+//    <time>16:01</time>
+//    <version>0.1</version>
+//    <author></author>
+//    <description></description>
+//-----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1
+{
+    internal class Program
+    {
+        #region ZonaDeTeste 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+        #endregion
+    }
+}
